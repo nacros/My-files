a788cc10402fd50db90d69e451049a96c96c8bd3
nacros <31615199+nacros@users.noreply.github.com>
2017-10-30 16:05:45 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -2,8 +2,8 @@
 import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
-s.login("hemavathy10iadrt@gmail.com","hema")
-message="hi"
-s.sendmail("hemavathy10iadrt@gmail.com","hemavathy10iadrt@gmail.com",message)
+s.login("hemavathy10iadrt@gmail.com","hema") #sender's mail id
+message="hi"#message to be sent
+s.sendmail("hemavathy10iadrt@gmail.com","hemavathy10iadrt@gmail.com",message) #reciver's mail id
 s.quit()
 
